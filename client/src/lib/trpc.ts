26d1b2ac84107308bd62de5383682e2ced0fcd07
rdefranc-hub <rdefranc@gmail.com>
2025-11-07 10:45:51 -0300

    Update trpc.ts

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,18 @@
-import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "../../../server/routers";
+// @ts-nocheck
+
+import { createTRPCReact } from '@trpc/react-query';
+import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import superjson from 'superjson';
+
+export type AppRouter = any;
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export const trpcClient = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: '/api/trpc'
+    })
+  ],
+  transformer: superjson
+});
